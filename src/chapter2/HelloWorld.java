cbcd730cb3d20301ea73918f76e919eaccdaf80b
Hai Ng. <nguyenhai0527@gmail.com>
2025-09-14 22:19:40 +0700

    Add personal details to HelloWorld program

--- src/chapter2/HelloWorld.java
+++ src/chapter2/HelloWorld.java
@@ -2,8 +2,18 @@ package chapter2;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        byte age = 25;
+        float height = 5.9f;
+        char initial = 'J';
+        boolean isStudent = true;
+
         System.out.println("Hello, World! and welcome to Java programming.");
         System.out.println("This is my first Java program.");
         System.out.println("I am excited to learn more about Java.");
+        System.out.println("I am " + age + " years old.");
+        System.out.println("I am graduating in 2025.");
+        System.out.println("My height is " + height + " feet.");
+        System.out.println("My first name starts with the letter " + initial + ".");
+        System.out.println("Am I a student? " + isStudent);
     }
 }
