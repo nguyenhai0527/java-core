d02853d2c032643048fd1765516e37bcde1e6f22
Hai Ng. <nguyenhai0527@gmail.com>
2025-09-14 21:59:04 +0700

    Hello World!

---
+++ src/chapter2/HelloWorld.java
@@ -0,0 +1,9 @@
+package chapter2;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! and welcome to Java programming.");
+        System.out.println("This is my first Java program.");
+        System.out.println("I am excited to learn more about Java.");
+    }
+}
